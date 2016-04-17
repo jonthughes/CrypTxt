9f2652e97feb65f7edd5cb5d68dda5f8e29f2742
Jon <jonthughes@hotmail.com>
2016-04-17 12:20:55 -0700

    Created basic navigation and layouts

--- app/src/main/java/uw/edu/tcss450/team6/cryptxt/HomeActivity.java
+++ app/src/main/java/uw/edu/tcss450/team6/cryptxt/HomeActivity.java
@@ -1,7 +1,9 @@
 package uw.edu.tcss450.team6.cryptxt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void startInbox(View view) {
+        Intent intent = new Intent(HomeActivity.this, InboxActivity.class);
+        startActivity(intent);
+    }
+
+    public void startContacts(View view) {
+        Intent intent = new Intent(HomeActivity.this, ContactsActivity.class);
+        startActivity(intent);
+    }
+
+    public void startSend(View view) {
+        Intent intent = new Intent(HomeActivity.this, SendActivity.class);
+        startActivity(intent);
+    }
+
+    public void startShare(View view) {
+        Intent intent = new Intent(HomeActivity.this, ShareActivity.class);
+        startActivity(intent);
+    }
+
+    public void startHelp(View view) {
+        Intent intent = new Intent(HomeActivity.this, HelpActivity.class);
+        startActivity(intent);
     }
 }
